1a9da8e67b790d6176591915961d3e61ee252276
hotelos2026 <hotelos2026@gmail.com>
2026-03-18 10:46:53 +0000

    fix: force sanctum model connection

--- backend-api/app/Providers/AppServiceProvider.php
+++ backend-api/app/Providers/AppServiceProvider.php
@@ -3,8 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\Tenant;             // <--- Ajoute ceci
-use App\Observers\TenantObserver;   // <--- Ajoute ceci
+use App\Models\Tenant;
+use App\Observers\TenantObserver;
+use Laravel\Sanctum\PersonalAccessToken; // <--- Ajoute ceci
+use Laravel\Sanctum\Sanctum;             // <--- Ajoute ceci
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // C'EST ICI QUE LA MAGIE OPÈRE
+        // 1. Gestion de la création automatique des schémas
         Tenant::observe(TenantObserver::class);
+
+        // 2. FORCE Sanctum à utiliser le modèle standard 
+        // Cela permet de s'assurer qu'il respecte la connexion 'default' 
+        // que nous changeons dans le Middleware IdentifyTenant.
+        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
     }
 }
